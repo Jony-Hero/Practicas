5b2b2b4ce6d87644b746933717043514776b4fde
Jony-Hero <jonathanruizoliva@gmail.com>
2020-12-22 09:00:38 +0100

    Funcion de sumar

---
+++ funcion.java
@@ -0,0 +1,15 @@
+package Funciones;
+
+public class funcion {
+	
+	
+	public static double sumar(double m , double n) {
+		double z = 0;
+		
+		z = m+n;
+		System.out.println(z);
+		return z;
+	}
+	
+
+}
